364d127d2dc414d1e39ee9a4b821c8fa18153c16
Tenzin Choedak <tchoedak@gmail.com>
2020-03-24 00:43:43 -0700

    integrate serialization into pykeyval, make set and delete return booleans to indicate success

--- pykeyval/pykeyval.py
+++ pykeyval/pykeyval.py
@@ -1,38 +1,47 @@
 from sqlalchemy.engine.url import make_url
-from sqlalchemy import create_engine
+from . import serializers
 
 
 class PyKeyVal(object):
+    def __init__(self, url, name, namespace=None, serializer=None, **options):
+        self.url = url
+        self.name = name
+        self.namespace = namespace
+        self.options = options
+        self.serializer = serializer or JsonSerializer
+        self.stores = {'snowflake': SnowKeyVal, 'dict': DictKeyVal}
+        self._store = self.stores.get(options.get('store')) or self.stores.get(
+            self._store_from_url(url)
+        )  # loads the underlying store Object
 
-	def __init__(self, url, name, namespace=None, **options):
-		self.url = url
-		self.name = name
-		self.namespace = namespace
-		self.options = options
-		self.stores = {
-			'snowflake': SnowKeyVal
-		}
-		self._store = self.stores.get(options.get('store')) or self._store_from_url(url) # loads the underlying store Object
-		self.engine = create_engine(url)
+        if self._store:
+            self.store = self._store(
+                url=self.url, name=self.name, namespace=self.namespace, **options
+            )
+        else:
+            raise ValueError(
+                'Unable to find an adequate store. Please use one of', self.stores
+            )
 
-		if self._store:
-			self.store = store(engine=self.engine, name=self.name, namespace=self.namespace**options)
-		else:
-			raise ValueError('Unable to find an adequate store. Please use one of', self.stores)
+    def _store_from_url(self, url):
+        backend = make_url(url).get_backend_name()
+        return backend
 
-	def _store_from_url(self, url):
-		backend = make_url(url).get_backend_name()
-		return backend
+    def _serialize(self, val):
+        return self.serializer.serialize(dict(_value=val))
 
-	def set(self, key, value, ttl=None):
-		self.store.set(key, value)
+    def _deserialize(self, val):
+        return self.serializer.deserialize(val).get('_value')
 
-	def get(self, key):
-		self.store.set(key)
+    def set(self, key, val):
+        self.store.set(key, self._serialize(val))
+        return True
 
-	def delete(self, key):
-		self.store.delete(key)
+    def get(self, key):
+        return self._deserialize(self.store.get(key))
 
-	def clear(self):
-		self.store.clear()
+    def delete(self, key):
+        return self.store.delete(key)
 
+    def clear(self):
+        self.store.clear()
--- pykeyval/snow_keyval.py
+++ pykeyval/snow_keyval.py
@@ -3,102 +3,99 @@ from sqlalchemy import Table, MetaData, String, Column
 from sqlalchemy.orm import sessionmaker, mapper
 from sqlalchemy.ext.declarative import declarative_base
 from snowflake.sqlalchemy import VARIANT
-from sqlalchemy import func
+from sqlalchemy import func, create_engine
 
 
 def generate_keyval_model(table, tablename):
+    class KeyVal(object):
+        def __init__(self, key, val):
+            __tablename__ = tablename
+            self.key = key
+            self.val = val
 
-	class KeyVal(object):
-		def __init__(self, key, val):
-			__tablename__ = tablename
-			self.key = key
-			self.val = val
-
-	mapper(KeyVal, table)
-	return KeyVal
+    mapper(KeyVal, table)
+    return KeyVal
 
 
 class SnowKeyVal(object):
+    def __init__(self, url, name, namespace=None, **options):
+        self.engine = create_engine(url)
+        self._connection = None
+        self._session = None
+        self.meta = MetaData(self.engine)
+        self.name = name
+        self.namespace = namespace
+        self.tablename = self.get_tablename(name, namespace)
+        self.table = Table(
+            self.tablename,
+            self.meta,
+            Column('key', String, primary_key=True),
+            Column('val', String),
+        )
+        self.table.create(checkfirst=True)
+        self.KeyVal = generate_keyval_model(self.table, self.tablename)
+        self.local_cache = {}
 
-	def __init__(self, engine, name, namespace=None, **options):
-		self.engine = engine
-		self._connection = None
-		self._session = None
-		self.meta = MetaData(self.engine)
-		self.name = name
-		self.namespace = namespace
-		self.tablename = self.get_tablename(name, namespace)
-		self.table = Table(
-			self.tablename,
-			self.meta,
-			Column('key', String, primary_key=True),
-			Column('val', VARIANT)
-		)
-		#self.create_table(self.tablename)
-		self.table.create(checkfirst=True)
-		self.KeyVal = generate_keyval_model(self.table, self.tablename)
-		self.local_cache = {}
+    def get_tablename(self, name, namespace=None):
+        tablename = f'{name}'
+        if namespace:
+            tablename += f'_{namespace}'
+        return tablename
 
-	def get_tablename(self, name, namespace=None):
-		tablename = f'{name}'
-		if namespace:
-			tablename += f'_{namespace}'
-		return tablename
-
-	def create_table(self, tablename):
-		stmt = f'''
+    def create_table(self, tablename):
+        stmt = f'''
 		CREATE TABLE IF NOT EXISTS {tablename}
 		(
 		key STRING,
 		val VARIANT
 		)
 		'''
-		self.connection.execute(stmt)
+        self.connection.execute(stmt)
 
-	@property
-	def session(self):
-		if self._session:
-			return self._session
-		else:
-			Session = sessionmaker()
-			Session.configure(bind=self.engine)
-			self._session = Session()
-			return self._session
+    @property
+    def session(self):
+        if self._session:
+            return self._session
+        else:
+            Session = sessionmaker()
+            Session.configure(bind=self.engine)
+            self._session = Session()
+            return self._session
 
-	@property
-	def connection(self):
-		if self._connection:
-			return self._connection
-		else:
-			self._connection = self.engine.connect()
-			return self._connection
+    @property
+    def connection(self):
+        if self._connection:
+            return self._connection
+        else:
+            self._connection = self.engine.connect()
+            return self._connection
 
-	def set(self, key, val):
-		item = self._get(key)
-		if item.first():
-			print('has val')
-			item.val = val
-			self.session.commit()
-		else:
-			print('creating val')
-			item = self.KeyVal(key=key, val=val)
-			self.session.add(item)
-			self.session.commit()
+    def set(self, key, val):
+        item = self._get(key)
+        item_result = item.first()
+        if item_result:
+            item_result.val = val
+            self.session.commit()
+        else:
+            item = self.KeyVal(key=key, val=val)
+            self.session.add(item)
+            self.session.commit()
 
-	def _get(self, key):
-		val = self.session.query(self.KeyVal).filter(
-			self.KeyVal.key==key
-		)
-		return val
+    def _get(self, key):
+        val = self.session.query(self.KeyVal).filter(self.KeyVal.key == key)
+        return val
 
-	def get(self, key):
-		return self._get(key).first().val
+    def get(self, key):
+        return self._get(key).first().val
 
-	def delete(self, key):
-		val = self._get(key)
-		val.delete()
-		self.session.commit()
+    def delete(self, key):
+        val = self._get(key)
+        key_exists = val.first() is not None
+        if key_exists:
+            val.delete()
+            self.session.commit()
+        return key_exists
 
-	def clear(self):
-		self.session.query(self.KeyVal).delete()
-		self.session.commit()
+    def clear(self):
+        self.session.query(self.KeyVal).delete()
+        self.session.commit()
