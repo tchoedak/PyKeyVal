2cddad89c91d4c2efb645b0acee520823c10e171
Tenzin Choedak <tchoedak@gmail.com>
2020-08-19 00:25:06 -0700

    add dependencies on redis, sqlalchemy, and snowflake

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=pkgs,
     description='',
     author='tchoedak@gmail.com',
-    install_requires=requirements,
+    install_requires=['redis>=3.3.11', 'SQLAlchemy>=1.3.3', 'snowflake-sqlalchemy>=1.1.16'],
     tests_require=['pytest'],
     test_suite="tests",
 )
