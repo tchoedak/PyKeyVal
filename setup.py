17b3d20ece210cbfe59cc1dc0196e8db8aa3c8e9
Tenzin Choedak <tchoedak@gmail.com>
2020-08-25 01:04:22 -0700

    add long descriptions for pypi readability

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+import pathlib
 from setuptools import setup, find_packages
 
-import pykeyval
+
 pkgs = ['pykeyval.%s' %s for s in find_packages('./pykeyval')]
 pkgs.append('pykeyval')
 
-
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='pykeyval',
     version='0.0.1',
     packages=pkgs,
-    description='',
+    description='PyKeyVal is a key-value store that you can pack in a bag and take with you.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='tchoedak@gmail.com',
     install_requires=requirements,
     setup_requires=['pytest-runner'],
